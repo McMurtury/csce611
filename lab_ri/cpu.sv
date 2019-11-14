b78ea83e5271f5fb695b1fcc31f1a1724c7bf5e6
BUTLER <cnbutler@cocsce-l3d22-05.ds.sc.edu>
2019-11-14 18:04:45 -0500

    Updating files

--- lab_ri/cpu.sv
+++ lab_ri/cpu.sv
@@ -39,10 +39,11 @@ module cpu (
 	//regfile signals
 	logic rdrt_EX;
 	logic [1:0] lo_CD;
-	logic [31:0] readdata2_EX;
+	logic [31:0] readdata2_EX, readdata1_EX;
 
 	//alu signals
 	logic [1:0] alu_src_EX;
+	logic A_BP;
 
 	//read file
 	initial begin
@@ -58,8 +59,12 @@ module cpu (
 	assign B_EX = alu_src_EX == 2'b0 ? readdata2_EX :
 		      alu_src_EX == 2'b1 ?
 		      {{16{instruction_EX[15]}},instruction_EX[15:0]} :
+		      alu_src_EX == 2'b10 ? lo_WB :
 		      {16'b0, instruction_EX[15:0]};
 	
+
+	//A bypass mux
+	assign A_EX = A_BP == 1'b1 ? lo_WB : readdata1_EX;
 
 	//pc control
 	logic [1:0] pc_src_EX;
@@ -96,18 +101,16 @@ module cpu (
 			regwrite_WB <= 1'b0;
 		end else begin
 			regwrite_WB <= regwrite_EX;
-			if (rdrt_EX == 1'b0) writeaddr_WB <= instruction_EX[15:11];
-			else if (rdrt_EX == 1'b1) writeaddr_WB <= instruction_EX[20:16];
-			//writeaddr_WB <= rdrt_EX == 1'b0 ?  instruction_EX[15:11] : instruction_EX[20:16];
-			//if(lo_CD == 2'b0) lo_WB <= lo_EX;
-			//else if (lo_CD == 2'b1) lo_WB <= hi_EX;
-			//else if (lo_CD == 2'b10) lo_WB <= lo_EX;
+			/*if (rdrt_EX == 1'b0) writeaddr_WB <= instruction_EX[15:11];
+			else if (rdrt_EX == 1'b1) writeaddr_WB <= instruction_EX[20:16];*/
+			writeaddr_WB <= rdrt_EX == 1'b0 ?  instruction_EX[15:11] : instruction_EX[20:16];
 		end
 	end
+
 	//for lo_EX
 	always_ff @(posedge clk,posedge rst) begin
-			if(lo_CD == 2'b0) lo_WB <= lo_EX;
-			else if (lo_CD == 2'b1) lo_WB <= hi_EX;
+			if(lo_CD == 2'b0) lo_WB <= lo_EX; //<=
+			else if (lo_CD == 2'b1) lo_WB <= hi_EX; //<=
 			//else if (lo_CD == 2'b10) lo_WB <= lo_EX;
 	end
 
@@ -116,7 +119,7 @@ module cpu (
 			   //execute (decode)
 			   .readaddr1(instruction_EX[25:21]),
 			   .readaddr2(instruction_EX[20:16]),
-			   .readdata1(A_EX),
+			   .readdata1(readdata1_EX),
 			   .readdata2(readdata2_EX),
 
 			   //writeback
@@ -139,9 +142,9 @@ module cpu (
 		stall_FETCH = 1'b0;
 		GPIO_out_en = 1'b0;
 		op_EX = 4'b0100;
-		//regwrite_EX <= 1'b0;
 		shamt_EX = 5'bXXXXX;
 		lo_CD = 2'b0;
+		A_BP = 1'd0;
 		if(rst == 1) regwrite_EX = 1'b0;
 
 		if(~stall_EX) begin
@@ -150,55 +153,117 @@ module cpu (
 				if (instruction_EX[5:0] == 6'b100000 || 
 					instruction_EX[5:0] == 6'b100001) begin //add, addu
 					regwrite_EX = 1'b1;
+					rdrt_EX = 1'b1;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
 				end else if (instruction_EX[5:0] == 6'b100010 ||
 						instruction_EX[5:0] == 6'b100011) begin//sub, subu
 					op_EX = 4'b0101;
 					regwrite_EX = 1'b1;
-				end else if (instruction_EX[5:0] == 6'b011000 ||
-						instruction_EX[5:0] == 6'b011001) begin//mult, multu
+					rdrt_EX = 1'b1;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
+				end else if (instruction_EX[5:0] == 6'b011000) begin//mult
 					op_EX = 4'b0110;
 					regwrite_EX = 1'b1;
+					rdrt_EX = 1'b1;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
+				end else if (instruction_EX[5:0] == 6'b011001) begin//multu
+					op_EX = 4'b0111;
+					regwrite_EX = 1'b1;
+					rdrt_EX = 1'b1;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
 				end else if (instruction_EX[5:0] == 6'b100100) begin//and
 					op_EX = 4'b0000;
 					regwrite_EX = 1'b1;
+					rdrt_EX = 1'b1;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
 				end else if (instruction_EX[5:0] == 6'b100101) begin//or
 					op_EX = 4'b0001;
 					regwrite_EX = 1'b1;
+					rdrt_EX = 1'b1;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
 				end else if (instruction_EX[5:0] == 6'b100110) begin//xor
 					op_EX = 4'b0011;
 					regwrite_EX = 1'b1;
+					rdrt_EX = 1'b1;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
 				end else if (instruction_EX[5:0] == 6'b100111) begin//nor
 					op_EX = 4'b0010;
 					regwrite_EX = 1'b1;
+					rdrt_EX = 1'b1;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
 				end else if (instruction_EX[5:0] == 6'b000000) begin//sll
 					op_EX = 4'b1000;
-					regwrite_EX <= 1'b1;
+					regwrite_EX = 1'b1;
 					shamt_EX = instruction_EX[10:6];
+					rdrt_EX = 1'b1;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
 				end else if (instruction_EX[5:0] == 6'b000010) begin//srl
 					op_EX = 4'b0111;
 					regwrite_EX = 1'b1;
 					shamt_EX = instruction_EX[10:6];
+					rdrt_EX = 1'b1;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
 				end else if (instruction_EX[5:0] == 6'b000011) begin//sra
 					op_EX = 4'b101X;
 					regwrite_EX = 1'b1;
 					shamt_EX = instruction_EX[10:6];
+					rdrt_EX = 1'b1;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
 				end else if (instruction_EX[5:0] == 6'b101010) begin//slt
 					op_EX = 4'b1100;
 					regwrite_EX = 1'b1;
+					rdrt_EX = 1'b1;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
 				end else if (instruction_EX[5:0] == 6'b101011) begin//sltu
 					op_EX = 4'b11XX;
-					regwrite_EX <= 1'b1;
+					regwrite_EX = 1'b1;
+					rdrt_EX = 1'b1;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
 				end else if (instruction_EX[5:0] == 6'b010000) begin//mfhi
-					regwrite_EX <= 1'b1;
+					regwrite_EX = 1'b1;
 					rdrt_EX = 1'b0;
 					lo_CD = 2'b1;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
 				end else if (instruction_EX[5:0] == 6'b010010) begin//mflo
-					regwrite_EX <= 1'b1;
+					regwrite_EX = 1'b1;
 					rdrt_EX = 1'b0;
 					lo_CD = 2'b10;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
 				end else if (instruction_EX[31:0] == 32'b0) begin//nop
 					stall_FETCH = 1;
 					regwrite_EX = 1'b0;
+					if (instruction_EX[20:16] == writeaddr_WB) alu_src_EX = 2'b10;
+					if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
 				end 
 
 			//i-types instructions
@@ -208,7 +273,7 @@ module cpu (
 					
 					regwrite_EX = 1'b1;
 					alu_src_EX = 2'b10;
-					rdrt_EX = 1'b0;
+					rdrt_EX = 1'b1;
 			//lui
 			end else if (instruction_EX[31:26]==6'b001111) begin
 					op_EX = 4'b1000;
@@ -236,6 +301,28 @@ module cpu (
 						instruction_EX[10:6]==5'b0) begin
 						GPIO_out_en = 1'b1;
 						regwrite_EX = 1'b0;
+
+			end else if (instruction_EX[31:26] == 6'b001100) begin//andi
+				op_EX = 4'b0000;
+				alu_src_EX = 2'b11;
+				regwrite_EX = 1'b1;
+				rdrt_EX = 1'b1;
+				if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
+			end else if (instruction_EX[31:26] == 6'b001110) begin//xori
+				op_EX = 4'b0011;
+				alu_src_EX = 2'b11;
+				regwrite_EX = 1'b1;
+				rdrt_EX = 1'b1;
+				if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
+			end else if (instruction_EX[31:26] == 6'b001010) begin//slti
+				op_EX = 4'b1100;
+				alu_src_EX = 2'b11;
+				regwrite_EX = 1'b1;
+				rdrt_EX = 1'b1;
+				if (instruction_EX[25:21] == writeaddr_WB) A_BP = 1'd1;
+
 			end
 		end
 	end
